4468c453c96364fd4d4bea7d0f7ff6d40a9ef3fc
Pavel Kuzmich <pavel_127@mail.ru>
2022-01-30 22:48:13 +0300

    ADDED project TestTask

---
+++ TestTask/TestTask/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TestTask
+//
+//  Created by macbook on 30.01.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ TestTask/TestTask/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by macbook on 30.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+
+
+
+
+
+
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
