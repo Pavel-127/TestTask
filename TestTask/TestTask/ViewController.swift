d58b3018c9185345dd12338fdaa7e9286839be5f
Pavel Kuzmich <pavel_127@mail.ru>
2022-01-31 02:38:08 +0300

    ADDED imageView

--- TestTask/TestTask/ViewController.swift
+++ TestTask/TestTask/ViewController.swift
@@ -7,8 +7,20 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    var conteinerView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Vector")
+        image.contentMode = .scaleAspectFill
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+
+
+
+        return image
+    }()
 
 
 
@@ -22,7 +34,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
+        view.addSubview(conteinerView)
+
+        updateConstraints()
+
     }
 
+    func updateConstraints() {
+        conteinerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        conteinerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        conteinerView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        conteinerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 }
 
