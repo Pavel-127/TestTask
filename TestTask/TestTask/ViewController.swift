8ab8834e92c17a9a05d97b939b8acd60da9fe65d
Pavel Kuzmich <pavel_127@mail.ru>
2022-02-01 00:27:06 +0300

    ADDED view, stakView and textfield

--- TestTask/TestTask/ViewController.swift
+++ TestTask/TestTask/ViewController.swift
@@ -31,24 +31,70 @@ class ViewController: UIViewController {
     var label: UILabel = {
         let label = UILabel()
         label.text = "ЛОКАЦИИ"
+        label.textAlignment = .center
+        label.font = UIFont(name: "Oswald", size: 15)
         label.font = label.font.withSize(32)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
+    var view1: UIView = {
+        let view = UIView()
+//        view.spacing = 15
+        view.layer.cornerRadius = 10
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: 3, height: 3)
+        view.layer.shadowOpacity = 0.7
+        view.layer.shadowRadius = 4.0
+//        view.axis = .horizontal
+//        view.distribution = .fillEqually
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
 
+        return view
+    }()
+
+    var stackView: UIStackView = {
+        let stakView = UIStackView()
+        stakView.spacing = 15
+        stakView.layer.cornerRadius = 10
+        stakView.axis = .horizontal
+        stakView.distribution = .fillEqually
+        stakView.translatesAutoresizingMaskIntoConstraints = false
+        stakView.backgroundColor = .lightGray
+
+        return stakView
+    }()
+
+    var textField: UITextField = {
+        let txtField = UITextField()
+        txtField.placeholder = "Название локации"
+        txtField.translatesAutoresizingMaskIntoConstraints = false
+        return txtField
+    }()
+
+    var addButton: UIButton = {
+        let button = UIButton()
+        button.image(for: .normal)
+        return button
+    }()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
         view.addSubview(conteinerView)
         view.addSubview(conteinerView1)
         view.addSubview(label)
+        view.addSubview(view1)
+        view.addSubview(stackView)
+        view.addSubview(textField)
         updateConstraints()
         updateConstraints1()
         updateConstraints2()
-
+        updateConstraints3()
+        updateConstraints4()
+        updateConstraints5()
     }
 
     func updateConstraints() {
@@ -73,5 +119,28 @@ class ViewController: UIViewController {
         label.topAnchor.constraint(equalTo: view.topAnchor, constant: 115).isActive = true
         conteinerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
+
+    func updateConstraints3() {
+        view1.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        view1.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        view1.topAnchor.constraint(equalTo: view.topAnchor, constant: 250).isActive = true
+        view1.heightAnchor.constraint(equalToConstant: 300).isActive = true
+//        stack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    func updateConstraints4() {
+        textField.leftAnchor.constraint(equalTo: stackView.leftAnchor, constant: 5).isActive = true
+        textField.rightAnchor.constraint(equalTo: stackView.rightAnchor, constant: -60).isActive = true
+        textField.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 2).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 30).isActive = true
+//        stack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    func updateConstraints5() {
+        stackView.leftAnchor.constraint(equalTo: view1.leftAnchor, constant: 15).isActive = true
+        stackView.rightAnchor.constraint(equalTo: view1.rightAnchor, constant: -15).isActive = true
+        stackView.topAnchor.constraint(equalTo: view1.topAnchor, constant: 15).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view1.bottomAnchor, constant: -15).isActive = true
+//        stack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+
 }
 
